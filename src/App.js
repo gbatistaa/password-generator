1c2688a381a5bbc7fe150e6dde03513d923ea4f2
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-03-02 18:25:15 -0300

    começo da adição dos elementos de configuração de senha

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ function App() {
   return (
     <div className="App">
       <p className="title">Passsword Generator</p>
-      <div className=""></div>
+      <div className="password-space"></div>
+      <div className="password-settings">
+        <div className="length-info">
+          <p className="mini-name">Caracther length</p>
+          <p className="length-number"></p>
+        </div>
+      </div>
     </div>
   );
 }
