2bef912d821a6f3ffcf5c32e126549b165b542f3
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-03-02 21:59:21 -0300

    adicionado o compnente de força da senha e o botão de generação

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import Strength from "./Components/Strength";
+import Including from "./Components/Including";
 
 function App() {
   return (
@@ -8,8 +10,14 @@ function App() {
       <div className="password-settings">
         <div className="length-info">
           <p className="mini-name">Caracther length</p>
-          <p className="length-number"></p>
+          <p className="length-number">10</p>
         </div>
+        <input type="range" className="length-range" />
+        <Including />
+        <Strength />
+        <button type="submit" className="generate-btn">
+          GENERATE
+        </button>
       </div>
     </div>
   );
