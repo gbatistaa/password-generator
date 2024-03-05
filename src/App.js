8d2db2d8e469b2854047500c23b8eae451e65092
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-03-05 11:26:21 -0300

    configuração do range max e min, configuração e construção da função de alterar o valor do range e sincronização do paragraph com o state de range

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
+import { useState } from "react";
 import GeneratorButton from "./Components/GeneratorButton";
 import Strength from "./Components/Strength";
 import Including from "./Components/Including";
 
 function App() {
+  const [lengthRange, setLengthRange] = useState(0);
+  const [lengthPercentage, setLengthPercentage] = useState(0);
+
+  const handleRangeInputChange = (e) => {
+    e.preventDefault();
+
+    const range = e.target;
+    const min = range.min;
+    const max = range.max;
+    const passwordLength = range.value;
+    setLengthRange(passwordLength);
+    const percentage = ((passwordLength - min) * 100) / (max - min);
+    setLengthPercentage(percentage);
+  };
+
   return (
     <div className="App">
       <p className="title">Passsword Generator</p>
@@ -11,9 +27,17 @@ function App() {
       <div className="password-settings">
         <div className="length-info">
           <p className="mini-name">Caracther length</p>
-          <p className="length-number">10</p>
+          <p className="length-number">{lengthRange}</p>
         </div>
-        <input type="range" className="length-range" />
+        <input
+          type="range"
+          className="length-range"
+          min="0"
+          max="25"
+          value={lengthRange}
+          style={{ backgroundSize: `${lengthPercentage}% 100%` }}
+          onChange={(e) => handleRangeInputChange(e)}
+        />
         <Including />
         <Strength />
         <GeneratorButton />
