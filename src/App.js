15bfe57a235e077d698a9a5f71d2b5ed5cbcbc4a
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-03-06 08:17:07 -0300

    realocação dos provedores de context para melhor acesso dos states pelos componentes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import "./App.css";
 import { useState, createContext } from "react";
 import { CharTypesProvider } from "./CharTypesContext.js";
-import { passwordStrengthLogic } from "./logic.js";
 import GeneratorButton from "./Components/GeneratorButton";
 import Strength from "./Components/Strength";
 import Including from "./Components/Including";
 import RangeLength from "./Components/RangeLength.js";
 
 export const LengthContext = createContext();
+export const StrengthContext = createContext();
 
 function App() {
   const [lengthRange, setLengthRange] = useState(6);
+  const [strengthState, setStrengthState] = useState(0);
 
   return (
     <div className="App">
@@ -23,11 +24,15 @@ function App() {
         </div>
         <CharTypesProvider>
           <LengthContext.Provider value={{ lengthRange, setLengthRange }}>
-            <RangeLength />
+            <StrengthContext.Provider
+              value={{ strengthState, setStrengthState }}
+            >
+              <RangeLength />
+              <Including />
+              <Strength />
+              <GeneratorButton />
+            </StrengthContext.Provider>
           </LengthContext.Provider>
-          <Including />
-          <Strength />
-          <GeneratorButton />
         </CharTypesProvider>
       </div>
     </div>
