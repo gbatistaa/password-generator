b21929fd4bf445ed9ce70283f5e222c8a2ba95c2
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-03-06 20:33:20 -0300

    mudança do nome de uma variável e criação da função que analisa qual cor das level bar baseada na força da senha

--- src/logic.js
+++ src/logic.js
@@ -1,8 +1,19 @@
 export const passwordStrengthLogic = (characthersTypeObject, length) => {
-  let charactersStrength = parseInt(length);
+  let passwordStrength = parseInt(length);
   for (const state in characthersTypeObject) {
-    if (characthersTypeObject[state]) charactersStrength += 2;
+    if (characthersTypeObject[state]) passwordStrength += 2;
   }
-  console.log(charactersStrength);
-  return charactersStrength;
+  return passwordStrength;
+};
+
+export const passwordStrengthColor = (passwordStrength) => {
+  if (passwordStrength <= 13) {
+    return "#ff0000";
+  } else if (passwordStrength <= 20) {
+    return "#ff7f00";
+  } else if (passwordStrength <= 27) {
+    return "#ffff00";
+  } else {
+    return "#5ACA75";
+  }
 };
