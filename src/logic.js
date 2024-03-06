a9206b6707793ea8b20e532de8c191f5fd0b2836
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-03-06 08:13:56 -0300

    resolução do bug da função de calcular força da senha

--- src/logic.js
+++ src/logic.js
@@ -1,7 +1,8 @@
-const passwordStrengthLogic = (characthersTypeObject, length) => {
-  let charactersStrength = 0;
+export const passwordStrengthLogic = (characthersTypeObject, length) => {
+  let charactersStrength = parseInt(length);
   for (const state in characthersTypeObject) {
     if (characthersTypeObject[state]) charactersStrength += 2;
   }
-  return length + charactersStrength;
+  console.log(charactersStrength);
+  return charactersStrength;
 };
