b87c90d81e0575d452f306455a7d08878915af65
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-03-14 16:23:56 -0300

    adicionada função de lidar com o clique de geração da senha, com verificação de erro e importação do state da senha

--- src/Components/GeneratorButton.js
+++ src/Components/GeneratorButton.js
@@ -1,12 +1,24 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { useCharTypes } from "../CharTypesContext.js";
+import { passwordCreatorLogic } from "../logic.js";
+import { PassowrdContext } from "../App.js";
 import arrow from "../assets/arrow-right-solid.svg";
 import blackArrow from "../assets/arrow-right-solid-black.svg";
 
 function GeneratorButton() {
   const [hoverState, setHoverState] = useState(false);
+  const [blockedState, setBlockedState] = useState(false);
+  const { passwordState, setPasswordState } = useContext(PassowrdContext);
   const { userIncludings } = useCharTypes();
-  const { lower, upper, numbers, symbols } = userIncludings;
+  //const { lower, upper, numbers, symbols } = userIncludings;
+
+  const verifyEveryTypeIsFalse = (userIncludings) => {
+    let everyTypeFalse = true;
+    for (const type in userIncludings) {
+      if (userIncludings[type]) everyTypeFalse = false;
+    }
+    return everyTypeFalse;
+  };
 
   const handleHoverButtonMouseEnter = (e) => {
     e.preventDefault();
@@ -18,12 +30,21 @@ function GeneratorButton() {
     setHoverState(false);
   };
 
+  const handleButtonClick = (e) => {
+    e.preventDefault();
+    const isToBlock = verifyEveryTypeIsFalse(userIncludings);
+    setBlockedState(isToBlock);
+    const newPassword = passwordCreatorLogic(userIncludings);
+    setPasswordState(newPassword);
+  };
+
   return (
     <button
       type="submit"
-      className="generate-btn"
+      className={`generate-btn ${blockedState ? "no-type" : ""}`}
       onMouseEnter={(event) => handleHoverButtonMouseEnter(event)}
       onMouseLeave={(event) => handleHoverButtonMouseLeave(event)}
+      onClick={(event) => handleButtonClick(event)}
     >
       GENERATE
       <img
