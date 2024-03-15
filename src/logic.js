d46f764c630d2cd0416608f03f47494ee3cc0b98
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-03-14 22:48:31 -0300

    construção da função de lógica de criação da senha

--- src/logic.js
+++ src/logic.js
@@ -1,11 +1,13 @@
-const uppercaseLetters = Array.from({ length: 26 }, (_, i) =>
-  String.fromCharCode(65 + i)
-);
 const lowercaseLetters = Array.from({ length: 26 }, (_, i) =>
   String.fromCharCode(97 + i)
 );
+const uppercaseLetters = Array.from({ length: 26 }, (_, i) =>
+  String.fromCharCode(65 + i)
+);
 const algarisms = Array.from({ length: 10 }, (_, i) => String(i));
 const specialCharacters = ["!", "@", "#", "$", "%", "&", "*", "-"];
+
+let matrix = [lowercaseLetters, uppercaseLetters, algarisms, specialCharacters];
 
 export const passwordStrengthLogic = (characthersTypeObject, length) => {
   let passwordStrength = parseInt(length);
@@ -27,10 +29,24 @@ export const passwordStrengthColor = (passwordStrength) => {
   }
 };
 
-export const passwordCreatorLogic = ({ lower, upper, numbers, symbols }) => {
+export const passwordCreatorLogic = (userIncludings, passwordLength) => {
   let auxiliarArray = [];
-  if (lower) auxiliarArray.push(...lowercaseLetters);
-  if (upper) auxiliarArray.push(...uppercaseLetters);
-  if (numbers) auxiliarArray.push(...algarisms);
-  if (symbols) auxiliarArray.push(...specialCharacters);
+  let index = 0;
+  for (const type in userIncludings) {
+    if (userIncludings[type]) auxiliarArray.push(...matrix[index]);
+    index++;
+  }
+
+  let newPassword = "";
+  const auxLength = auxiliarArray.length;
+  let randomIndex, pastIndex;
+  for (let i = 0; i < passwordLength; i++) {
+    pastIndex = randomIndex;
+    randomIndex = Math.floor(Math.random() * auxLength);
+    while (randomIndex === pastIndex) {
+      randomIndex = Math.floor(Math.random() * auxLength);
+    }
+    newPassword += auxiliarArray[randomIndex];
+  }
+  return newPassword;
 };
